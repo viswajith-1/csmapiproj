db0703b3f40ada967d9fd2d89a59fd8244e0fe49
viswajithsb <viswajithsaji@gmail.com>
2025-06-18 17:19:07 +0530

    viswajith complete

---
+++ csmapiproj/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'csmdb',
+        'HOST': 'djangemsfaith2.cnqui8gi2qtf.us-east-2.rds.amazonaws.com',
+        'USER': 'admin',
+        'PASSWORD': 'viswajith123',
+        'PORT': 3306
+    }
+}

dedad1eba7fc69623ea3b70bea05315720930e60
sangeeth7736 <sangeethselvan.25@gmail.com>
2025-06-18 22:38:58 +0530

    complete

---
+++ csmapiproj/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'csmdb',
+        'USER':'root',
+        'PASSWORD':'sangeeth@223',
+        'HOST':'localhost',       
+        'PORT':3306,
+    }
+}
